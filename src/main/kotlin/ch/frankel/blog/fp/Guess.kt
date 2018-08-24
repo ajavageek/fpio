e77115d65f2d17eaa16d878e65d2dd1a370c66f6
Nicolas Frankel <nicolas@frankel.ch>
2018-08-24 15:43:18 +0200

    Replace for comprehensions by IO companion object

--- src/main/kotlin/ch/frankel/blog/fp/Guess.kt
+++ src/main/kotlin/ch/frankel/blog/fp/Guess.kt
@@ -1,7 +1,9 @@
 package ch.frankel.blog.fp
 
-import arrow.core.*
-import arrow.effects.*
+import arrow.core.Try
+import arrow.effects.IO
+import arrow.effects.fix
+import arrow.effects.monad
 import arrow.typeclasses.binding
 import java.security.SecureRandom
 
@@ -16,39 +18,34 @@ fun main(args: Array<String>) {
 
 private fun nextInt(upper: Int): IO<Int> = IO { random.nextInt(upper) }
 
-private fun gameLoop(name: String?): IO<Unit> = ForIO extensions {
-    binding {
-        val number = nextInt(5).map { it + 1 }.bind()
-        putStrLn("Dear $name, please guess a number from 1 to 5:").bind()
-        val input = getStrLn().bind()
-        parseInt(input).fold(
-                { putStrLn("You did not enter a number!").bind() },
-                {
-                    if (it == number) putStrLn("You guessed right, $name!").bind()
-                    else putStrLn("You guessed wrong, $name! The number was $number").bind()
-                }
-        )
-        val cont = checkContinue(name).bind()
-        if (cont) gameLoop(name)
-        else Unit.liftIO()
-    }
-    .flatten()
-    .fix()
+private fun gameLoop(name: String?): IO<Unit> = IO.monad().binding {
+    val number = nextInt(5).map { it + 1 }.bind()
+    putStrLn("Dear $name, please guess a number from 1 to 5:").bind()
+    val input = getStrLn().bind()
+    parseInt(input).fold(
+            { putStrLn("You did not enter a number!").bind() },
+            {
+                if (it == number) putStrLn("You guessed right, $name!").bind()
+                else putStrLn("You guessed wrong, $name! The number was $number").bind()
+            }
+    )
+    val cont = checkContinue(name).bind()
+    if (cont) gameLoop(name).bind()
+    else Unit
 }
+.fix()
 
-private fun checkContinue(name: String?): IO<Boolean> = ForIO extensions {
-    binding {
-        putStrLn("Do you want to continue, $name?").bind()
-        val input = getStrLn().map { it?.toLowerCase() }.bind()
-        when(input) {
-            "y" -> true.liftIO()
-            "n" -> false.liftIO()
-            else -> checkContinue(name)
-        }
+
+private fun checkContinue(name: String?): IO<Boolean> = IO.monad().binding {
+    putStrLn("Do you want to continue, $name?").bind()
+    val input = getStrLn().map { it?.toLowerCase() }.bind()
+    when (input) {
+        "y" -> true
+        "n" -> false
+        else -> checkContinue(name).bind()
     }
-    .flatten()
-    .fix()
 }
+.fix()
 
 private fun putStrLn(line: String): IO<Unit> = IO { println(line) }
 private fun getStrLn(): IO<String?> = IO { readLine() }
