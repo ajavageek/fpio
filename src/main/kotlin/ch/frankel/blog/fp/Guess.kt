71f398b2f3ee0131f81e63c3711ec1052a14d3c5
Nicolas Frankel <nicolas@frankel.ch>
2018-08-24 12:39:22 +0200

    Correctly manage number reading partial function

--- src/main/kotlin/ch/frankel/blog/fp/Guess.kt
+++ src/main/kotlin/ch/frankel/blog/fp/Guess.kt
@@ -1,5 +1,6 @@
 package ch.frankel.blog.fp
 
+import arrow.core.*
 import java.security.SecureRandom
 
 private val random = SecureRandom()
@@ -12,9 +13,14 @@ fun main(args: Array<String>) {
     while(exec) {
         val number = random.nextInt(5) + 1
         println("Dear $name, please guess a number from 1 to 5:")
-        val guess = readLine()?.toInt()
-        if (guess == number) println("You guessed right, $name!")
-        else println("You guessed wrong, $name! The number was $number")
+        val guess: Try<Int?> = parseInt(readLine())
+        when(guess) {
+            is Failure -> println("You did not enter a number!")
+            is Success<Int?> -> {
+                if (guess.value == number) println("You guessed right, $name!")
+                else println("You guessed wrong, $name! The number was $number")
+            }
+        }
         println("Do you want to continue, $name?")
         when(readLine()) {
             "y" -> exec = true
@@ -22,3 +28,5 @@ fun main(args: Array<String>) {
         }
     }
 }
+
+private fun parseInt(input: String?) = Try {input?.toInt() }
