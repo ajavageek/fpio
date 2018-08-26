474d58f990046d39670d3724c7dc6111302aebcd
Nicolas Frankel <nicolas@frankel.ch>
2018-08-26 16:46:30 +0200

    Final result

--- src/main/kotlin/ch/frankel/blog/fp/Guess.kt
+++ src/main/kotlin/ch/frankel/blog/fp/Guess.kt
@@ -11,20 +11,24 @@ import java.security.SecureRandom
 private val random = SecureRandom()
 
 fun main(args: Array<String>) {
-    println("What is your name?")
-    val name = readLine()
-    println("Hello, $name, welcome to the game!")
-    gameLoop(name).unsafeRunSync()
+    mainIO(args).unsafeRunSync()
 }
 
+private fun mainIO(args: Array<String>): IO<Unit> = IO.monad().binding {
+    putStrLn("What is your name?").bind()
+    val name = getStrLn().bind()
+    putStrLn("Hello, $name, welcome to the game!").bind()
+    gameLoop(name).bind()
+}.fix()
+
 private fun gameLoop(name: String?): IO<Unit> = IO.monad().binding {
-    println("Dear $name, please guess a number from 1 to 5:")
-    (readLine() as String).safeToInt().fold(
-            { println("You did not enter a number!") },
+    putStrLn("Dear $name, please guess a number from 1 to 5:").bind()
+    getStrLn().safeToInt().fold(
+            { putStrLn("You did not enter a number!").bind() },
             {
-                val number = random.nextInt(5) + 1
-                if (it == number) println("You guessed right, $name!")
-                else println("You guessed wrong, $name! The number was $number")
+                val number = nextInt(5).map { it + 1 }.bind()
+                if (it.bind() == number) println("You guessed right, $name!")
+                else putStrLn("You guessed wrong, $name! The number was $number").bind()
             }
     )
     checkContinue(name).map {
@@ -36,16 +40,19 @@ private fun gameLoop(name: String?): IO<Unit> = IO.monad().binding {
 
 
 private fun checkContinue(name: String?): IO<Boolean> = IO.monad().binding {
-    println("Do you want to continue, $name?")
-    (readLine() as String).trueMap { it.toLowerCase() }.trueMap {
+    putStrLn("Do you want to continue, $name?").bind()
+    (getStrLn()).map { it.toLowerCase() }.map {
         when (it) {
             "y" -> true.liftIO()
             "n" -> false.liftIO()
             else -> checkContinue(name)
         }
-    }.bind()
+    }.flatten()
+    .bind()
 }.fix()
 
-private fun <T> String.trueMap(f: (String) -> T) = f(this)
+private fun putStrLn(line: String): IO<Unit> = IO { println(line) }
+private fun getStrLn(): IO<String> = IO { readLine() as String }
 
-private fun String.safeToInt() = Try { this.toInt() }
+private fun nextInt(upper: Int): IO<Int> = IO { random.nextInt(upper) }
+private fun IO<String>.safeToInt() = Try { map { it.toInt() }}
